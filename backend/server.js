386111911a4925dc98e65d1f8bee7502800970a2
jobshastra2025 <jobshastra2025@gmail.com>
2025-06-02 08:55:00 +0530

    removed helmet

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import eventRoutes from "./routes/event.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import cors from "cors";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import helmet from "helmet";
+
 
 dotenv.config();
 
@@ -24,7 +24,6 @@ app.use(
   })
 );
 
-app.use(helmet());
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
